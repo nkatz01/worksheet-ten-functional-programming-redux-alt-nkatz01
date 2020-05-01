647c13f9402c886a49b128cdba5ac1534d725946
nkatz01 <nkatz01@dcs.bbk.ac.uk>
2020-05-02 00:13:07 +0100

    Question1 redone - tests left todo

---
+++ worksheet10/QuestionOne.Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+ 
+
+namespace QuestionOne.Test
+
+     [TestClass]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+       public void 
+    }
+}
--- worksheet10/QuestionOne/Program.cs
+++ worksheet10/QuestionOne/Program.cs
@@ -1,44 +1,69 @@
 ﻿using System;
- 
+
 
 namespace QuestionOne
 {
     class Program
     {
-      
+
         static void Main(string[] args)
         {
-         
+
             Console.WriteLine("Please enter weight in kg: ");
             string w = Console.ReadLine();
             Console.WriteLine("Please enter height in meters: ");
             string h = Console.ReadLine();
-            Bmi bmi = new Bmi(Convert.ToDouble(w), Convert.ToDouble(h));
+            //  Bmi bmi = new Bmi(Convert.ToDouble(w), Convert.ToDouble(h));
+            var func = CalcBmi();
+            Console.WriteLine(PrintRes(func(Convert.ToDouble(w), Convert.ToDouble(h))));
 
-            Console.WriteLine(PrintRes(bmi));
-            Console.WriteLine(bmi);
+
         }
 
+        public static string PrintRes(double? bmi) => !(bmi < 18.5 || bmi >= 25) ? "Healthy weight" : ((bmi < 18.5 ? "Underweight" : "Overweight"));
 
-        public static string PrintRes(Bmi? bmi) =>  !(bmi.CalcBmi() < 18.5 || bmi.CalcBmi() >= 25) ? "Healthy weight" : ((bmi.CalcBmi() < 18.5 ? "Underweight" : "Overweight"));
-    }
+        //   public static string PrintRes(Bmi? bmi) => !(bmi.CalcBmi() < 18.5 || bmi.CalcBmi() >= 25) ? "Healthy weight" : ((bmi.CalcBmi() < 18.5 ? "Underweight" : "Overweight"));
 
-   public   class Bmi
-    {
 
-        public double Weight { get; }
-        public double Height { get; }
-
-        public Bmi(double weight, double height)
+        public static Func<double, double, double> CalcBmi()
         {
-            Weight = weight;
-            Height = height;
+
+            return (w, h) => w / Math.Pow(h, 2);
+
+
+
 
 
         }
 
-        public double CalcBmi() => Weight/( Math.Pow(Height, 2));
 
-        
+
+        //   public double CalcBmi() => Weight / (Math.Pow(Height, 2)); //in main
+
+
+
+
+
+        //public   class Bmi
+        // {
+
+        //     public double Weight { get; }
+        //     public double Height { get; }
+
+        //     public Bmi(double weight, double height)
+        //     {
+        //         Weight = weight;
+        //         Height = height;
+
+
+        //     }
+
+        //     public double CalcBmi() => Weight/( Math.Pow(Height, 2));
+
+
+        // }
+
+
+
     }
 }

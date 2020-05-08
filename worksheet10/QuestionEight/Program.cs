669f2451e2a2fd854d84600397e1200bcbb0e916
nkatz01 <nkatz01@dcs.bbk.ac.uk>
2020-05-08 17:41:30 +0100

    done question 8

--- worksheet10/QuestionEight/Program.cs
+++ worksheet10/QuestionEight/Program.cs
@@ -1,78 +1,123 @@
 ﻿using LanguageExt;
+using LanguageExt.TypeClasses;
+using LanguageExt.UnsafeValueAccess;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using static LanguageExt.Prelude;
 
 namespace QuestionEight
 {
-    class Program
+    public class Program
     {
+ 
+        public delegate Option<TA> MyDelegate<TA>(IEnumerable<TA> collec, Func<TA, bool> pred);
         static void Main(string[] args)
         {
             Employee personA = new Employee("person1", DateTime.Today.AddYears(1));
             Employee personB = new Employee("person2", DateTime.Today.AddDays(-1));
-            Console.WriteLine($"{personA.Id} is employed from {personA.WorkPermit.IssueDate} to {personA.WorkPermit.IssueDate}");
+            Console.WriteLine($"{personA.Id} is employed from {personA.WorkPermit.IssueDate} to {personA.WorkPermit.ValidUntil}");
+            Console.WriteLine($"{personB.Id} is employed from {personB.WorkPermit.IssueDate} to {personB.WorkPermit.ValidUntil}");
+ 
             IDictionary<string, Employee> ienum = new Dictionary<string, Employee> {
                 {personA.Id, personA },
-                { personB.Id, personB  } 
-            
-                };
-            var result3 = myDict.MySelect<KeyValuePair<string, int>, KeyValuePair<string, int>>((pair) => new KeyValuePair<string, int>("number " + pair.Key, pair.Value + 1));
+                { personB.Id, personB  }
 
-            ienum.MySelect(Find( .Key.Equals("person1"), );//<KeyValuePair<string, Employee>, KeyValuePair<string, Employee>>
-            Console.WriteLine();
+                };
+
+
+ 
+            MyDelegate<KeyValuePair<string, Employee>> func = new MyDelegate<KeyValuePair<string, Employee>>(SetMethod.filterListOnPred);
+            //   var result1 = func(ienum, x => x.Key.Equals("person1"));
+
+            var result1 = ienum.MySelect(x => x.Key.Equals("person1"), func);
+            var result2 = ienum.MySelect(x => x.Key.Equals("person2"), func);
+            var result3 = ienum.MySelect(x => x.Key.Equals("person3"), func);
+
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+
         }
 
-
-        static Option<WorkPermit> GetWorkingPermit(Dictionary<string, Employee> people, string employeeId)
-         {
-             Employee value;
+        public static Option<WorkPermit> GetWorkingPermit(Dictionary<string, Employee> people, string employeeId)
+        {
+            Employee value;
             if (people.TryGetValue(employeeId, out value))
                 return Some(value.WorkPermit);
             else
-                return None;//TryGetValue(employeeId, out value)//x => x.Key.Equals(employeeId)).ToList.Count() > 0 
+                return None;
         }
+
+
     }
+
     public static class SetMethod
     {
+        public static Option<TA> filterListOnPred<TA>(this IEnumerable<TA> Ienum, Func<TA, bool> Predicate) => Ienum.Find(Predicate);
 
-        public static Option<TB> MySelect<TA, TB>(this IEnumerable<TA> Ienum, Predicate<TA> pred  Func<IEnumerable<TA>, TB> map)
-        
-
-
-
-          =>  Ienum.Find(x =>  pred(x)) != null ? map(Ienum.Find(x => pred(x))) : None;
-
-     //   Ienum.Where(i => map(i).Equals(true)).ToList().Count() > 0 ? Some((TB) Ienum.Where(i => map(i).Equals(true)).ToList()[0]) : None;
+        public static Option<WorkPermit> MySelect(this IEnumerable<KeyValuePair<string, Employee>> Ienum, Func<KeyValuePair<string, Employee>, bool> pred, Program.MyDelegate<KeyValuePair<string, Employee>> extractEmp)
+        { 
 
 
+            extractEmp(Ienum, pred);
 
-         
+            var record = extractEmp(Ienum, pred);
+
+            var workpermit = record.Match(Some: i => Some(i.Value.WorkPermit), None: () => None);
+
+            var tru = workpermit.Match(Some: i => i.ValidUntil > DateTime.Today, None: () => false);
+           
+           
+            if (tru)
+                return workpermit;
+            else
+                return
+                    None;
+
+
+
+
+
+        }
+
+
+        //public static Option<WorkPermit> MySelect<TA>(this IEnumerable<TA> Ienum, Func<TA, bool> pred, Program.MyDelegate<TA> extractEmp)
+        //{
+
+        //        var Dictrecord = extractEmp(Ienum, pred);
+        //        var wrkprmt = Dictrecord.Match(Some: i => (Employee)i.Value.WorkPermit, None: i => None);
+        //        return wrkprmt;
+
+        //    }
+        // ? Some(Ienum.AsQueryable().Where(Predicate).ToList() ) : None ;
+        //Func<IEnumerable<TA>, Func<TA, bool>,  Option<TA>> extractEmp
+
     }
 
     public class Employee
     {
-       public WorkPermit WorkPermit { get; }
-       public string Id { get; }
+        public WorkPermit WorkPermit { get; }
+        public string Id { get; }
         public Employee(string id, DateTime validUntil)
         {
             Id = id;
             WorkPermit = new WorkPermit(DateTime.Today, validUntil);
-            
+
         }
 
-        
+
     }
 
     public class WorkPermit
     {
         public DateTime ValidUntil { get; }
         public DateTime IssueDate { get; }
-        public WorkPermit(DateTime issuedate, DateTime validuntil)   { IssueDate = issuedate; ValidUntil = validuntil; }
-       
+        public WorkPermit(DateTime issuedate, DateTime validuntil) { IssueDate = issuedate; ValidUntil = validuntil; }
+
     }
 
-   
+
 
 }

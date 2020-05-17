e4cb9bb515b2436a7fc947944f7ae43243680be1
nkatz01 <nkatz01@dcs.bbk.ac.uk>
2020-05-17 11:32:06 +0100

    done q5 PB

--- worksheet10/QuestionFivePB/Program.cs
+++ worksheet10/QuestionFivePB/Program.cs
@@ -10,16 +10,17 @@ namespace QuestionFivePB
         static void Main(string[] args)
         {
             IEnumerable<int> ls1 = new List<int>{ 3, 4, 5, 6, 7, 8 };
-            // var ls1 = ls.InsertAt(0, 3).InsertAt(1, 4).InsertAt(2, 5).InsertAt(3, 6).InsertAt(4, 7).InsertAt(5, 8);
-            // Console.WriteLine($"List: {ls1}");
-            //  var ls2 = ls1.InsertAt(5, 10);
-            // Console.WriteLine($"List: {ls2}");
-            var ls3 = ls1.RemoveAt<int>(4);
+
+            Console.WriteLine("({0})", string.Join(",", ls1));
+            var ls2 = ls1.MyInsertAt(5, 10);
+            Console.WriteLine("({0})", string.Join(",", ls2));
+            var ls3 = ls2.MyRemoveAt<int>(4);
             Console.WriteLine("({0})", string.Join(",", ls3));
-            //var ls4 = ls3.TakeWhile(x => x <= 5);
-            //Console.WriteLine($"List: {ls4}");
-            //var ls5 = ls3.DropWhile(x => x <= 5);
-            //Console.WriteLine($"List: {ls5}");
+            var ls4 = ls3.MyTakeWhile(x => x <= 5);
+            Console.WriteLine("({0})", string.Join(",", ls4));
+
+            var ls5 = ls3.MyDropWhile(x => x <= 5);
+            Console.WriteLine("({0})", string.Join(",", ls5));
 
         }
 
@@ -38,17 +39,24 @@ namespace QuestionFivePB
     {
 
 
-        public static IEnumerable<T> RemoveAt<T>(this IEnumerable<T> lst, int m)
-        {
-
-            //    var t = lst.GetType();
-            // IEnumerable<T> NewIEnumerable = Activator.CreateInstance(t) as IEnumerable<T>;
-            return m == 0 || lst.Count() < 1
+        public static IEnumerable<T> MyRemoveAt<T>(this IEnumerable<T> lst, int m) => m == 0 || lst.Count() < 1
                 ? lst.Skip(1)
-                : lst.Take(1).Concat(lst.Skip(1).RemoveAt(m - 1));
+                : lst.Take(1).Concat(lst.Skip(1).MyRemoveAt(m - 1));
+
+        public static IEnumerable<T> MyInsertAt<T>(this IEnumerable<T> lst,  int m, T value) => m == 0 || lst.Count() < 1
+               ? lst.Prepend(value)
+               : lst.Take(1).Concat(lst.Skip(1).MyInsertAt( m - 1, value));
 
 
-        }
+        public static IEnumerable<T> MyTakeWhile<T>(this IEnumerable<T> lst, Func<T, bool> pred) => !pred(lst.First()) || lst.Count() < 1
+            ? new List<T>()
+            : lst.Take(1).Concat(lst.Skip(1).MyTakeWhile(pred));
+
+        public static IEnumerable<T> MyDropWhile<T>(this IEnumerable<T> lst, Func<T, bool> pred) =>    lst.Count() < 1 || !pred(lst.First())
+            ? lst
+              //   : lst.Skip(1).Concat(lst.Skip(1).MyDropWhile(pred));
+              : lst.Skip(1).MyDropWhile(pred);
+
 
     }
 
